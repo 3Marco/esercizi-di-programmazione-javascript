449fd961273feaac3f4f48be1e59bb9c320253f8
3Marco <113450912+3Marco@users.noreply.github.com>
2022-09-20 13:53:43 +0200

    Update caldo-o-freddo.js

--- esercizi-condizioni/caldo-o-freddo.js
+++ esercizi-condizioni/caldo-o-freddo.js
@@ -9,3 +9,17 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+//Fatto
+
+var a = 10;
+var b = -2;
+var c = 31;
+var d = 22;
+var e = 15;
+var f = -6;
+var g = 7;
+var max = Math.max(a, b, c, d, e, f, g);
+var min = Math.min(a, b, c, d, e, f, g);
+console.log('giornata più calda = ' + max + ', giornata più fredda = ' + min);
