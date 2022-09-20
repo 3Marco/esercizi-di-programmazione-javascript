eadd5d8e6a07a012066da66da0505f82db2d1318
Marco Bernasconi <113450912+3Marco@users.noreply.github.com>
2022-09-20 14:16:23 +0200

    Update somma-e-media.js

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,14 @@
   
   http://www.imparareaprogrammare.it
 */
+
+//Fatto
+
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var somma = a+b+c+d+e;
+var media = somma/5;
+console.log('la somma è: ' + somma + ' ; la media è :' + media)
