2ce1607e8995aa992506b9718b609e5e1ed30aed
Marco Bernasconi <113450912+3Marco@users.noreply.github.com>
2022-09-21 11:43:31 +0200

    Update chi-la-azzecca.js

--- esercizi-condizioni/chi-la-azzecca.js
+++ esercizi-condizioni/chi-la-azzecca.js
@@ -19,3 +19,33 @@
 
   http://www.imparareaprogrammare.it
 */
+
+//Fatto
+
+var giocatore1 = 45;
+var giocatore2 = 30;
+var max = 100;
+var min = 1;
+var numcaus = Math.floor(Math.random() * ((max-min) + min));
+console.log('numero casuale generato = ' + numcaus)
+var difgioc1 = Math.abs(giocatore1 - numcaus);
+var difgioc2 = Math.abs(giocatore2 - numcaus);
+
+console.log('La differenza del giocatore 1 è: ' + difgioc1);
+console.log('La differenza del giocatore 2 è: ' + difgioc2);
+
+
+
+
+if (giocatore1 == numcaus) {
+  console.log('Complimenti al giocatore 1!!! Hai azzeccato il numero casuale!');
+}
+else if (giocatore2 == numcaus) {
+  console.log('Complimenti al giocatore 2!!! Hai azzeccato il numero casuale!');
+}
+else if (difgioc1 < difgioc2) {
+  console.log('Nessuno dei due giocatori ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più!');
+}
+else if (difgioc1 > difgioc2) {
+  console.log('Nessuno dei due giocatori ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più!');
+}
