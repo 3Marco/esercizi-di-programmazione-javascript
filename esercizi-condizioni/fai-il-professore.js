998676d53b07df09180673425f2c552ff3fcfdd9
Marco Bernasconi <113450912+3Marco@users.noreply.github.com>
2022-09-21 10:54:01 +0200

    Update fai-il-professore.js

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -18,3 +18,29 @@
 
   http://www.imparareaprogrammare.it
 */
+
+//Fatto
+
+var voto = 22
+
+  if (voto < 18) {
+    console.log('insufficiente');
+}
+  else if (18 <= voto && voto < 21) {
+    console.log('sufficiente');
+}
+  else if (21 <= voto && voto < 24) {
+    console.log('buono');
+}
+  else if (24 <= voto && voto < 27) {
+    console.log('distinto');
+}
+  else if (27 <= voto && voto <= 29) {
+    console.log('ottimo');
+}
+  else if (voto === 30) {
+    console.log('eccellente');
+}
+  else {
+    console.log('Errore! Il voto inserito non esiste!')
+}
