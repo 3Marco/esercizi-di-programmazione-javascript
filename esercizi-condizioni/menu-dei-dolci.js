9b5f2519f02b2370942dcfb4c569a6e01d799f7f
Marco Bernasconi <113450912+3Marco@users.noreply.github.com>
2022-09-21 12:20:27 +0200

    Update menu-dei-dolci.js

--- esercizi-condizioni/menu-dei-dolci.js
+++ esercizi-condizioni/menu-dei-dolci.js
@@ -21,3 +21,36 @@
 
   http://www.imparareaprogrammare.it
 */
+
+//Fatto
+
+var menu = ('             MENU: \n\
+\n\
+             1. Tiramisù \n\
+             2. Torta della nonna \n\
+             3. Cheescake alla nutella \n\
+             4. Macedonia \n\
+           ');
+
+console.log(menu);
+
+var scelta = 7
+;
+console.log('La tua scelta è: ' + scelta)
+switch (scelta) {
+  case 1:
+    console.log('Hai scelto il dolce Tiramisù');
+    break;
+  case 2:
+    console.log('Hai scelto il dolce Torta della nonna');
+    break;
+  case 3:
+    console.log('Hai scelto il dolce Cheescake alla nutella');
+    break;
+  case 4:
+    console.log('Hai scelto il dolce Macedonia');
+    break;
+  default:
+    console.log('Dolce non disponibile!');
+
+}
