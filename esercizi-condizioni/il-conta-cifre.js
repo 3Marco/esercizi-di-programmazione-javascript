a210a047b49086a6c3acc7e39485afcf9c0f5dc0
Marco Bernasconi <113450912+3Marco@users.noreply.github.com>
2022-09-21 12:42:25 +0200

    Update il-conta-cifre.js

--- esercizi-condizioni/il-conta-cifre.js
+++ esercizi-condizioni/il-conta-cifre.js
@@ -11,3 +11,16 @@
 
   http://www.imparareaprogrammare.it
 */
+
+//Fatto
+
+var numero = 6;
+
+var n = numero.toString()
+
+if ((numero <= 9999) && (n.length === 1))
+console.log('Il numero ' + numero + ' è composto da ' + n.length + ' cifra.');
+
+else if ((numero <= 9999) && (n.length !== 1))
+
+console.log('Il numero ' + numero + ' è composto da ' + n.length + ' cifre.');
