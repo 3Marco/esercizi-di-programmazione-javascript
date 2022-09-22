f04e2590c8a45de7151ff2d371095fea74aa43cf
Marco Bernasconi <113450912+3Marco@users.noreply.github.com>
2022-09-22 19:07:02 +0200

    Update ti-ricordi-le-tabelline.js

--- esercizi-cicli/ti-ricordi-le-tabelline.js
+++ esercizi-cicli/ti-ricordi-le-tabelline.js
@@ -8,3 +8,17 @@
 
   http://www.imparareaprogrammare.it
 */
+
+//Fatto
+
+var max = 10;
+var min = 1;
+var numero = Math.floor(Math.random() * (max-min)+min);
+console.log('Ecco la tabellina del numero: ' + numero);
+
+var limit = 11;
+
+for (var i = 1; i < limit; i++) {
+  var tab = numero * i;
+  console.log(tab);
+}
