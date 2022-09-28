14a8d17f4f2dc4d1a82fa7a7db702d12bfb9538f
Marco Bernasconi <113450912+3Marco@users.noreply.github.com>
2022-09-28 22:21:54 +0200

    Update operazioni-tra-array.js

--- esercizi-array/operazioni-tra-array.js
+++ esercizi-array/operazioni-tra-array.js
@@ -19,3 +19,56 @@
 
   http://www.imparareaprogrammare.it
 */
+
+//Fatto
+
+var mx = 10;
+var mn = 1;
+var one = Array(10);
+var two = Array(10);
+
+
+for (var i = 0; i < one.length; i++) {
+  one[i] = Math.floor(Math.random() * (mx-mn)+mn);
+}
+for (var i = 0; i < two.length; i++) {
+  two[i] = Math.floor(Math.random() * (mx-mn)+mn);
+}
+console.log(one);
+console.log(two);
+
+var ope = ('\n\
+OPERAZIONI DISPONIBILI: \n\
+\n\
+1. addizione \n\
+2. sottrazione \n\
+3. moltiplicazione \n\
+4. divisione \n\
+\n');
+console.log(ope);
+var max = 5;
+var min = 1;
+var three = Array(10);
+cope = Math.floor(Math.random() * (max-min)+min);
+
+for (var i = 0; i < three.length; i++) {
+switch (cope) {
+  case 1:
+    three[i] = one[i]+two[i];
+    console.log('Scelta: ' + cope + '. somma');
+    break;
+  case 2:
+    three[i] = one[i]-two[i];
+    console.log('Scelta: ' + cope + '. sottrazione');
+    break;
+  case 3:
+    three[i] = one[i]*two[i];
+    console.log('Scelta: ' + cope + '. moltiplicazione');
+    break;
+  case 4:
+    three[i] = one[i]/two[i];
+    console.log('Scelta: ' + cope + '. divisione');
+    break;
+   }
+}
+console.log(three);
