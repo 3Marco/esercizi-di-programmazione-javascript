fca0f2833a84c4a7da371caf113be277be7b8ee6
Marco Bernasconi <113450912+3Marco@users.noreply.github.com>
2022-10-01 10:00:43 +0200

    Update tanti-numeri.js

--- esercizi-array/tanti-numeri.js
+++ esercizi-array/tanti-numeri.js
@@ -12,3 +12,31 @@
 
   http://www.imparareaprogrammare.it
 */
+
+//Fatto
+
+var exe = Array();
+var exef = Array();
+var tot = 0;
+var xa = 50;
+var na = 1;
+var x = 15;
+var n = 2;
+exe.length = Math.floor(Math.random() * (x-n)+n);
+console.log('Lunghezza Array: ' + exe.length);
+for (var i = 0; i < exe.length; i++) {
+  exe[i] = Math.floor(Math.random() * (xa-na)+na);
+  tot += exe[i];
+
+ };
+console.log('Array iniziale: ' + exe);
+  var md = tot/exe.length;
+  exef = exe.filter(function evenNumbers (item) {
+    return item < md
+  }
+);
+console.log('Somma degli elementi: '+ tot);
+console.log('Media degli elementi: '+ md);
+console.log('Elementi sotto la media: ' + exef);
+console.log('Quantità elementi sotto la media: ' + exef.length);
+console.log('Quantità elementi sopra la media: ' + (exe.length-exef.length));
